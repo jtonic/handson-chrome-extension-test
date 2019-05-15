7399c0d8bd74283df90c3aaa3e7bd686e3a1dbcd
jtonic <antonel.pazargic@gmail.com>
2019-05-15 08:33:50 +0300

    add npm, use jquery as a es module and styling with twitter

--- ext/popup.js
+++
@@ -1,9 +0,0 @@
-function init() {
-  const cookies = chrome.cookies.getAll();
-  if (Array.isArray(cookies) && cookies.length > 0) {
-    window.alert(`1st cookie is ${cookies[0]}`);
-  }
-  window.alert(`no cookies: `);
-}
-
-document.addEventListener("DOMContentLoaded", init);
---
+++ ext/src/js/popup.js
@@ -0,0 +1,8 @@
+import "/node_modules/jquery/dist/jquery.min.js";
+
+function init() {
+  let isArray = $.isArray([]);
+  window.alert(`is array? ${isArray}`);
+}
+
+document.addEventListener("DOMContentLoaded", init);
