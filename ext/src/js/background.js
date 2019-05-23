e3babcc35d4ca665884ed6cb2dd1ae34c92ab45f
jtonic <antonel.pazargic@gmail.com>
2019-05-23 06:46:13 +0300

    async from settings.json file. badge when an url is open in at least one tab

--- ext/src/js/background.js
+++ ext/src/js/background.js
@@ -1,5 +1,8 @@
-chrome.runtime.onInstalled.addListener(function() {
-  alert("extension installed");
+chrome.runtime.onInstalled.addListener(async function() {
+  const settingsPath = chrome.runtime.getURL("src/settings/settings.json");
+  const json = await fetch(settingsPath).then(result => result.json());
+  console.log("settings", settingsPath, json);
+
   chrome.notifications.create({
     type: "basic",
     iconUrl: "assets/drink_water16.png",
@@ -7,5 +10,24 @@ chrome.runtime.onInstalled.addListener(function() {
     message: "Everyday I'm Guzzlin'!",
     buttons: [{ title: "Keep it Flowing." }],
     priority: 0
+  });
+
+  chrome.tabs.onUpdated.addListener(function callback(_, changeInfo, _) {
+    console.log("changeInfo", changeInfo);
+    if (
+      changeInfo.url &&
+      changeInfo.url.startsWith("https://mail.google.com")
+    ) {
+      chrome.browserAction.setBadgeText({ text: "ON" });
+    }
+  });
+
+  chrome.tabs.onRemoved.addListener(function callback(_, _) {
+    chrome.tabs.query({ url: "*://mail.google.com/*" }, tabs => {
+      console.log("tabs", tabs);
+      if (tabs.length < 1) {
+        chrome.browserAction.setBadgeText({ text: "" });
+      }
+    });
   });
 });
